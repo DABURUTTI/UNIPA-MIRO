41702edfea4569fba39683418b97dbc4bd533a36
DABURUTTI <daburutti@DABU-NOTE>
2019-06-03 10:41:20 +0900

    授業に関するお知らせを取得ぢるように変更

--- Unipa-miro.rb
+++ Unipa-miro.rb
@@ -4,8 +4,10 @@ require 'nokogiri'
 @wait_time = 3
 @timeout = 4
 
-USERID = ''
-PASSWORD = ''
+USERID = gets.chomp
+PASSWORD = gets.chomp
+
+puts "ユーザーIDとパスワードでログイン中"
 
 options = Selenium::WebDriver::Chrome::Options.new
 options.add_argument('--headless')
@@ -19,6 +21,8 @@ wait = Selenium::WebDriver::Wait.new(timeout: @wait_time)
 
 driver.get('https://portal.sa.dendai.ac.jp/uprx/')
 
+puts "接続中"
+
 search_box = driver.find_element(:id, 'loginForm:userId')
 search_box2 = driver.find_element(:id, 'loginForm:password')
 search_btn = driver.find_element(:id, 'loginForm:loginButton')
@@ -27,15 +31,35 @@ search_box.send_keys USERID
 search_box2.send_keys PASSWORD
 search_btn.click
 
+
+#sleep(1)
+all_btn = driver.find_element(:xpath, '//*[@id="funcForm:tabArea"]/ul/li[3]')
+all_btn.click
+
+puts "タスク１を実行"
+
+#タブ切り替え
+
+#軽視開始順に切り替え
 sleep(1)
-all_btn = driver.find_element(:xpath, '//*[@id="funcForm:tabArea"]/ul/li[2]')
+all_btn = driver.find_element(:xpath, '//*[@id="funcForm:tabArea:2:order:j_idt332:0:j_idt334"]/div[3]')
+all_btn.click
+
+all_btn = driver.find_element(:xpath, '//*[@id="funcForm:tabArea:2:order:j_idt332:0:j_idt334_panel"]/div/ul/li[1]')
 all_btn.click
 
 sleep(1)
-all_btn = driver.find_element(:xpath, '//*[@id="funcForm:tabArea:1:j_idt399"]/span')
+all_btn = driver.find_element(:xpath, '//*[@id="funcForm:tabArea:2:j_idt347"]/div[2]')
 all_btn.click
 
-sleep(1)
+sleep(3)
+all_btn = driver.find_element(:xpath, '//*[@id="funcForm:tabArea:2:j_idt399"]/span')
+all_btn.click
+
+
+puts "タスク２を実行"
+
+sleep(3)
 doc = Nokogiri::HTML.parse(driver.page_source)
 
 m = 0
@@ -43,11 +67,17 @@ m = 0
 target = doc.xpath ('//*[@id="funcForm:tabArea:1:allScr"]')
 target = Nokogiri::HTML.parse(target.to_s)
 
+puts "更新を待機中"
+
 target.xpath('//*[@id="keiji"]').each do |keiji|
 
     m = m + 1
-    puts keiji.xpath('.//a').text
+    puts "No.#{m}: #{keiji.xpath('.//a').text}"
+
+    sleep(0.03)
 
 end
+
+puts "完了 [#{m}件]"
 
 driver.quit
